3c0413d16618418efbbc0c0994ef7b485405dce5
T4D4 <t4d4.icp@gmail.com>
2024-11-25 17:15:02 +0900

    add get from stdio

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,8 @@
 fn main() {
     println!("1+1=??");
     println!("??の値を入力してください：");
+    let mut ans_input = String::new(); // userからの回答を保持する変数
+    // 標準入力から1行取得し、ans_inputに格納
+    std::io::stdin().read_line(&mut ans_input).unwrap();
+    dbg!(&ans_input); // ans_inputの中身を表示
 }
